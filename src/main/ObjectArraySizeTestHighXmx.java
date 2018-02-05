94a59b08a04fb15dc051103857a7627dc6e473cf
Xin Meng <x.meng@outlook.com>
2018-02-05 22:22:37 +0000

    Initial commit

---
+++ src/main/ObjectArraySizeTestHighXmx.java
@@ -0,0 +1,32 @@
+/**
+ * User:    mengxin
+ * Date:    05/02/2018
+ * Project: java-feature-test
+ * Package: science.mengxin.java
+ * Description: ObjectArraySizeTest.
+ *
+ * @author mengxin
+ * @version 1.0
+ */
+public class ObjectArraySizeTestHighXmx {
+    public static void main(String args[]) {
+        Integer arrayLength = Integer.MAX_VALUE;
+        Object[] objectProperties = null;
+
+        // objectProperties = new Object[2396729];
+        // System.out.print(objectProperties.length);
+        for (int i=arrayLength; i>1;i=i-1) {
+            try {
+                objectProperties = new Object[i];
+            } catch (OutOfMemoryError e) {
+                System.out.print("Out of memory: " + String.valueOf(i) + "\n");
+                // System.out.print(e.toString());
+            }
+            if (objectProperties != null) {
+                System.out.print("Find the limit by 1: " + String.valueOf(i) + "\n");
+                System.out.print(objectProperties.length + "\n");
+                break;
+            }
+        }
+    }
+}
